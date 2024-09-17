6165773e915fd8a907ab6fdc57414d3b29e5a236
KnamMarketing <neeraj.kumar@knam.in>
2024-09-17 10:34:08 +0530

    Update checkAccess.js

--- checkAccess.js
+++ checkAccess.js
@@ -1,20 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const user = JSON.parse(localStorage.getItem('user'));
-    const pageName = window.location.pathname.split('/').pop(); // Get the current page name
 
-    // Redirect to login if the user is not logged in or not authorized to access the page
-    if (!user) {
-        // Redirect only if the user is not already on the login page (index.html)
-        if (pageName !== 'index.html') {
-            window.location.href = 'index.html';
-        }
-    } else if (!user.access || !user.access.includes(pageName)) {
-        // If the user doesn't have access to the current page, redirect to index.html
-        if (pageName !== 'index.html') {
-            window.location.href = 'index.html';
-        }
-    }
-});
 
 
 
